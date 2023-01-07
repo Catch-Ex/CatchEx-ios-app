5866cfd969fe2f95938a36a90b2cbf8e59b60f99
김지훈 <ben224@naver.com>
2023-01-07 14:20:37 +0900

    [추가] 프로젝트 생성 및 spm 추가

---
+++ Catch-Ex-ios-app/Catch-Ex-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Catch-Ex-ios-app
+//
+//  Created by 김지훈 on 2023/01/07.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
+
